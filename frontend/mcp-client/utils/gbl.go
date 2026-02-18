5b318588b6885a0b6d1dc882f25ec74fb51a7b76
aarithi123 <148919216+aarithi123@users.noreply.github.com>
2026-02-17 20:22:42 -0500

    Implement MCP server utilities in gbl.go
    Add utility functions and types for MCP server configuration and responses.

---
+++ frontend/mcp-client/utils/gbl.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"github.com/ThinkInAIXYZ/go-mcp/client"
+	"github.com/tmc/langchaingo/llms/openai"
+)
+
+// MCP server configuration
+
+type MCPServerConfig struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+// MCPServerResponse holds metadata for routing
+
+type MCPServerResponse struct {
+	ServerID string
+	ToolName string
+	Response any
+}
+
+type AppConfig struct {
+	AppRoot   string `json:"app_root"`
+	LLMUrl    string `json:"llm_url"`
+	KUBEUrl   string `json:"kube_url"`
+	RTSUrl    string `json:"rts_url"`
+	SEARCHUrl string `json:"search_url"`
+	UFUrl     string `json:"uf_url"`
+
+	//MCPConfig []MCPConfig `json:"mcp_config"`
+}
+
+var (
+	AppRoot    string
+	mcpClients map[string]*client.Client
+	model      *openai.LLM
+)
+
+func GetMCPClients() map[string]*client.Client {
+	return mcpClients
+}
+
+func GetModel() *openai.LLM {
+	return model
+}
+
+func Stop() {
+	for _, c := range mcpClients {
+		c.Close()
+	}
+
+}

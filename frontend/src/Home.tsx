60ae3dbecfe8a21cbe26394c3f0b038ebd8d3c15
ErrianaT <thomaserriana@ufl.edu>
2026-02-22 17:20:10 -0500

    working on chatArea

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import NavBar from "./components/NavBar";
 import SidePanel from "./components/SidePanel";
+import ChatArea from "./components/chatArea";
 import "./Home.css";
 
 function Home() {
@@ -11,6 +12,9 @@ function Home() {
     <div className="app-container">
       <NavBar onMenuClick={toggleSidePanel} />
       <SidePanel isOpen={isSidePanelOpen} />
+      <div className="chat-wrapper">
+        <ChatArea />
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/chatArea.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ChatArea.css";
+
+const ChatArea: React.FC = () => {
+  return (
+    <div className="chat-box">
+      <h2>Welcome to the One-Stop Chat Assistant!</h2>
+    </div>
+  );
+};
+
+export default ChatArea;
